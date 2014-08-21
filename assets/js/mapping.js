b7d1a262aa6c3bbf19aa4bca7fa2908cf2fdcb49
Rubyist101 <teachystuff@yahoo.com>
2014-08-21 13:20:24 -0700

    last minute modifications/corrections

--- assets/js/mapping.js
+++ assets/js/mapping.js
@@ -80,7 +80,7 @@ var jekyllMap = (function (jekyllMap) {
 
 L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {id: 'examples.map-i86knfo3',   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="http://mapbox.com">Mapbox</a>'}).addTo(map);
+    'Imagery &#169; <a href="http://mapbox.com">Mapbox</a>'}).addTo(map);
 
     // Leaflet marker points creation, each marker on a new layer
     // Takes in generated .geojson file and adds to map
--- location/cesar.md
+++ location/cesar.md
@@ -1,13 +1,13 @@
 ---
-address: 555 Cesar Chavez Blvd., Los Angeles, CA 90012
+address: 555 W Cesar Chavez Blvd. Los Angeles, CA 90012
 title: Cesar Chavez Gardens
 install_date: 2003
 
 layout: location
 image: "Cesar-Chavez_MG_0066_800.jpg"
 
-latitude: 34.056479
-longitude: -118.231390
+latitude: 34.060131
+longitude: -118.242113
 
 linky: http://ltsc.org/index.php?option=com_content&view=article&id=339
 
